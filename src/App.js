92eff2a81adfb197aea8719b6daecb6f8b08760e
Lucas <luc4snoga@gmail.com>
2020-04-13 23:24:44 +0200

    Add image Card

--- src/App.js
+++ src/App.js
@@ -1,43 +1,42 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import ImageCard from "./components/imageCard";
+import ImageSearch from "./components/imageSearch";
 
 function App() {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [term, setTerm] = useState("");
+
+  useEffect(() => {
+    fetch(
+      `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`
+    )
+      .then(res => res.json())
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+      })
+      .catch(err => console.error(err));
+  }, [term]);
+
   return (
-    <React.Fragment>
-      <div className="max-w-sm rounded overflow-hidden shadow-lg">
-        <img
-          src="https://source.unsplash.com/random"
-          alt=""
-          className="w-full"
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-purple-500 text-xl mb-2">
-            Photo by John Doe
-          </div>
-          <ul>
-            <li>
-              <strong>Views: </strong> 4000
-            </li>
-            <li>
-              <strong>Downloads: </strong> 300
-            </li>
-            <li>
-              <strong>Likes: </strong> 400
-            </li>
-          </ul>
+    <div className="container mx-auto">
+      <ImageSearch searchText={text => setTerm(text)} />
+
+      {!isLoading && images.length === 0 && (
+        <h1 className="text-5xl text-center mx-auto">No images found</h1>
+      )}
+
+      {isLoading ? (
+        <h1 className="text-6xl text-center mx-auto">Loading</h1>
+      ) : (
+        <div className="grid grid-cols-3 gap-4">
+          {images.map(image => (
+            <ImageCard key={image.id} image={image} />
+          ))}
         </div>
-        <div className="px-6 py-4">
-          <span className="inline-block bg-gray-200 roubded-full px-3 py-1 text-sm font-semibold text-gray-7000 mr-2">
-            #tag1
-          </span>
-          <span className="inline-block bg-gray-200 roubded-full px-3 py-1 text-sm font-semibold text-gray-7000 mr-2">
-            #tag2
-          </span>
-          <span className="inline-block bg-gray-200 roubded-full px-3 py-1 text-sm font-semibold text-gray-7000 mr-2">
-            #tag3
-          </span>
-        </div>
-      </div>
-    </React.Fragment>
+      )}
+    </div>
   );
 }
 
